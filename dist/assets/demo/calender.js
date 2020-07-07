55fd0fc39d74199ed7613f593585ab5b547958ed
anand <anandgp935@gmail.com>
2020-07-07 19:24:02 +0530

    drop down fixed Signed-off-by: anand <anandgp935@gmail.com>

--- dist/assets/demo/calender.js
+++ dist/assets/demo/calender.js
@@ -49,32 +49,32 @@ $(function() {
 
 
 
-// $(function() {
+$(function() {
 
-//     // var start = moment().subtract(29, 'days');
-//     var start = moment().subtract(1, 'days');
-//     var end = moment().subtract(1, 'days');
+    // var start = moment().subtract(29, 'days');
+    var start = moment().subtract(1, 'days');
+    var end = moment().subtract(1, 'days');
 
-//     // var end
+    // var end
 
-//     function cb(start, end) {
-//         opens: 'center'
-//         $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-//     }
+    function cb(start, end) {
+        opens: 'center'
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
 
-//     $('#reportrange').daterangepicker(cb, {
-//         startDate: start,
-//         endDate: end,
-//         ranges: {
-//             'Today': [moment(), moment()],
-//             'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-//             'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-//             'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-//             'This Month': [moment().startOf('month'), moment().endOf('month')],
-//             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-//         }
-//     }, cb);
+    $('#reportrange').daterangepicker(cb, {
+        startDate: start,
+        endDate: end,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
 
-//     cb(start, end);
+    cb(start, end);
 
-// });
+});
